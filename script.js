342eb2a52fcc955c24c37b2e42b52c07cc7228ec
stranzerneeraz <stranzerneeraz12@gmail.com>
2023-07-13 18:22:06 +0100

    Menu Added

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function menuPage() {
+  window.location.href = "menu.html";
+}
+
 function showSignup() {
     var signupContainer = document.getElementById("signupContainer");
     signupContainer.style.display = "flex";
@@ -37,3 +41,43 @@ function loadMap() {
     markerStyle.textContent = '.marker { background-color: #f15a24; width: 24px; height: 24px; border-radius: 50%; cursor: pointer; }';
     document.head.appendChild(markerStyle);
 }
+
+
+// Remove Items From Cart
+$('a.remove').click(function(){
+  event.preventDefault();
+  $( this ).parent().parent().parent().hide( 400 );
+ 
+})
+
+// Just for testing, show all items
+  $('a.btn.continue').click(function(){
+    $('li.items').show(400);
+  })
+
+  $(document).ready(function() {
+
+    // Radio box border
+    $('.method').on('click', function() {
+      $('.method').removeClass('blue-border');
+      $(this).addClass('blue-border');
+    });
+  
+    // Validation
+    var $cardInput = $('.input-fields input');
+  
+    $('.next-btn').on('click', function(e) {
+  
+      $cardInput.removeClass('warning');
+  
+      $cardInput.each(function() {
+         var $this = $(this);
+  
+         if (!$this.val()) {
+           $this.addClass('warning');
+         }
+      });
+  
+    });
+  
+  });
